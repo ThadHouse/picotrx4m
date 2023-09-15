b778016bd751160807f8e41d4f393bf4b7ebea23
Thad House <thadhouse1@gmail.com>
2023-09-14 18:47:42 -0700

    Initial commit

---
+++ src/lights.rs
@@ -0,0 +1,122 @@
+use rp2040_hal::{
+    clocks::ClocksManager,
+    gpio::{DynPinId, FunctionPio0, Pin, PullDown},
+    pac::PIO0,
+    pio::{PIOBuilder, PinDir, Tx, UninitStateMachine, PIO, SM0},
+    Clock,
+};
+
+pub fn initialize_lights(
+    pio: &mut PIO<PIO0>,
+    sm: UninitStateMachine<(PIO0, SM0)>,
+    clocks: &ClocksManager,
+    pin: Pin<DynPinId, FunctionPio0, PullDown>,
+) -> Tx<(PIO0, SM0)> {
+    let program = pio_proc::pio_asm!(
+        ".define public t1 8", // High time at start
+        ".define public t2 6", // Delta
+        ".define public t3 8", // Low time at end
+        ".side_set 1",
+        "new_data:"
+        "pull       side 0 [0]",
+        "mov x osr  side 0 [0]",
+        "jmp !x do_stop side 0 [0]",
+        "out y, 1       side 0 [2]",
+        "jmp check_bit side 0 [0]",
+        "bitloop:",
+        "out y, 1       side 0 [t3 -1]",
+        "check_bit:",
+        "jmp !y do_zero side 1 [t1 -1]",
+        "do_one:",
+        "jmp !osre bitloop    side 1 [t2 -1]",
+        ".wrap",
+        "do_zero:",
+        "jmp !osre bitloop    side 0 [t2 - 1]",
+        ".wrap_target",
+        "jmp new_data       side 0 [0]",
+        "do_stop:",
+        "pull       side 0 [0]",
+        "mov x osr  side 0 [0]",
+        "keep_looping:",
+        "jmp x-- keep_looping   side 0 [7]", // TODO
+        "nop   side 1 [7]", // TODO
+        "jmp new_data       side 0 [0]", // TODO
+    );
+    let installed = pio.install(&program.program).unwrap();
+
+    let frequency = 871000;
+    let cycles_per_bit =
+        (program.public_defines.t1 + program.public_defines.t2 + program.public_defines.t3) as u32;
+    let frequency_per_bit = frequency * cycles_per_bit;
+
+    let int_part = clocks.system_clock.freq().to_Hz() / frequency_per_bit;
+    let remainder = clocks.system_clock.freq().to_Hz() % frequency_per_bit;
+    let fract_part = (remainder * 256) / frequency_per_bit;
+
+    let (mut sm, _, tx) = PIOBuilder::from_program(installed)
+        .side_set_pin_base(pin.id().num)
+        .out_shift_direction(rp2040_hal::pio::ShiftDirection::Right)
+        .autopull(false)
+        .pull_threshold(24)
+        .clock_divisor_fixed_point(int_part as u16, fract_part as u8)
+        .buffers(rp2040_hal::pio::Buffers::OnlyTx)
+        .build(sm);
+
+    sm.set_pindirs([(pin.id().num, PinDir::Output)]);
+
+    sm.start();
+
+    tx
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct FrontLeds {
+    pub yellow: u8,
+    pub low_beam: u8,
+    pub high_beam: u8,
+}
+
+impl From<FrontLeds> for u32 {
+    fn from(value: FrontLeds) -> Self {
+        let mut ret = 0xFF000000u32;
+        ret |= (value.high_beam as u32) << 16;
+        ret |= (value.low_beam as u32) << 8;
+        ret | (value.yellow as u32)
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct RearLeds {
+    pub yellow: u8,
+    pub white: u8,
+    pub red: u8,
+}
+
+impl From<RearLeds> for u32 {
+    fn from(value: RearLeds) -> Self {
+        let mut ret = 0xFF000000u32;
+        ret |= (value.red as u32) << 16;
+        ret |= (value.white as u32) << 8;
+        ret | (value.yellow as u32)
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
+pub struct Leds {
+    pub front_right: FrontLeds,
+    pub front_left: FrontLeds,
+    pub rear_right: RearLeds,
+    pub rear_left: RearLeds,
+}
+
+impl Leds {
+    pub fn write(&self, tx: &mut Tx<(PIO0, SM0)>) {
+        tx.write(self.front_left.into());
+        tx.write(self.front_right.into());
+        tx.write(self.rear_right.into());
+        tx.write(self.rear_left.into());
+        tx.write(0xFF000000u32);
+        tx.write(0);
+        tx.write(42);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,133 @@
+//! Blinks the LED on a Pico board
+//!
+//! This will blink an LED attached to GP25, which is the pin the Pico uses for the on-board LED.
+#![no_std]
+#![no_main]
+#![deny(unsafe_code)]
+#![deny(warnings)]
+
+use defmt::*;
+use defmt_rtt as _;
+use hal::{entry, gpio::FunctionPio0, prelude::_rphal_pio_PIOExt};
+use panic_probe as _;
+use rp2040_hal as hal;
+
+mod lights;
+
+// Provide an alias for our BSP so we can switch targets quickly.
+// Uncomment the BSP you included in Cargo.toml, the rest of the code does not need to change.
+// use sparkfun_pro_micro_rp2040 as bsp;
+
+use hal::{clocks::Clock, pac, watchdog::Watchdog};
+
+use crate::lights::{initialize_lights, FrontLeds, Leds, RearLeds};
+
+#[allow(unsafe_code)]
+#[link_section = ".boot2"]
+#[used]
+pub static BOOT2: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
+
+const XTAL_FREQ_HZ: u32 = 12_000_000u32;
+
+#[entry]
+fn main() -> ! {
+    info!("Program start");
+    let mut pac = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut watchdog = Watchdog::new(pac.WATCHDOG);
+
+    // Configure the clocks
+    let clocks = hal::clocks::init_clocks_and_plls(
+        XTAL_FREQ_HZ,
+        pac.XOSC,
+        pac.CLOCKS,
+        pac.PLL_SYS,
+        pac.PLL_USB,
+        &mut pac.RESETS,
+        &mut watchdog,
+    )
+    .ok()
+    .unwrap();
+
+    defmt::info!("{}", clocks.system_clock.freq().to_Hz());
+
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+    // The single-cycle I/O block controls our GPIO pins
+    let sio = hal::Sio::new(pac.SIO);
+
+    let pins = hal::gpio::Pins::new(
+        pac.IO_BANK0,
+        pac.PADS_BANK0,
+        sio.gpio_bank0,
+        &mut pac.RESETS,
+    );
+
+    let pin = pins
+        .gpio8
+        .into_push_pull_output_in_state(hal::gpio::PinState::Low)
+        .into_function::<FunctionPio0>()
+        .into_dyn_pin();
+
+    let (mut pio, sm0, _, _, _) = pac.PIO0.split(&mut pac.RESETS);
+
+    let mut tx = initialize_lights(&mut pio, sm0, &clocks, pin);
+
+    loop {
+        let leds = Leds {
+            front_right: FrontLeds {
+                yellow: 0,
+                low_beam: 0,
+                high_beam: 0,
+            },
+            front_left: FrontLeds {
+                yellow: 42,
+                low_beam: 0,
+                high_beam: 0,
+            },
+            rear_left: RearLeds {
+                yellow: 42,
+                white: 0,
+                red: 0,
+            },
+            rear_right: RearLeds {
+                yellow: 0,
+                white: 0,
+                red: 0,
+            },
+        };
+
+        leds.write(&mut tx);
+
+        delay.delay_ms(500);
+
+        let leds = Leds {
+            front_right: FrontLeds {
+                yellow: 0,
+                low_beam: 0,
+                high_beam: 0,
+            },
+            front_left: FrontLeds {
+                yellow: 0,
+                low_beam: 0,
+                high_beam: 0,
+            },
+            rear_left: RearLeds {
+                yellow: 0,
+                white: 0,
+                red: 0,
+            },
+            rear_right: RearLeds {
+                yellow: 0,
+                white: 0,
+                red: 0,
+            },
+        };
+
+        leds.write(&mut tx);
+
+        delay.delay_ms(500);
+    }
+}
+
+// End of file
